4f851cab1b2dee84cc75512d39b7d1de01b0cfc2
Akash Srivastava <akashsriv4all@gmail.com>
2024-01-03 13:29:54 +0530

    database_add

--- etl_project/etl_project/settings.py
+++ etl_project/etl_project/settings.py
@@ -75,12 +75,27 @@ WSGI_APPLICATION = 'etl_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": {
+        "ENGINE": "mssql",
+        "NAME": "ETL_DataBase",
+        "USER": "sa",
+        "PASSWORD": "sonata@123",
+        "HOST": "HODT153",
+        "PORT": "1433",
+        "OPTIONS": {
+            "driver": "ODBC Driver 17 for SQL Server",
+        },
+    },
 }
+
 
 
 # Password validation
